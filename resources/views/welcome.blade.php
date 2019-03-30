8991d9af64e1854d5df3ee4d52d9832c29b7ec2c
swapnilmonad <swapnil.1512@gmail.com>
2019-03-30 09:18:46 +0530

    registraion off

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
+                        @if ( false && Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
